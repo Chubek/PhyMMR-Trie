8ead26be325bfa20699a3b9a2f3a2e2c352d8b17
Chubak Bidpaa <chubakbidpaa@gmail.com>
2022-10-05 08:08:30 +0330

    fin

--- pytest.py
+++ pytest.py
@@ -7,20 +7,24 @@ from tqdm import tqdm
 def random_str(length):
     return "".join([ascii_letters[randint(0, len(ascii_letters) - 1)] for _ in range(length)])
 
+print("Generating key/value")
 
-key = random_str(randint(10, 1000))
-value = random_str(randint(1e2, 1e7))
+key = random_str(randint(42, 49))
+value = random_str(randint(300, 400))
+
 
 print(f"Key length: {len(key)}")
 print(f"Value length: {len(value)}")
 
 trie = PhyTrie()
 
-pbar = tqdm(total=1000)
+num_loop = 17696208
+
+pbar = tqdm(total=num)
 
 print("\nStarting test...\n")
 t1 = time()
-for _ in range(1000):
+for _ in range(num_loop):
     trie.insert(key, value)
     trie.get(key)
     pbar.update(1)
@@ -30,4 +34,4 @@ pbar.close()
 t2 = time()
 
 
-print(f"\n1000 inserts and gets done in {t2 - t1} seconds\n")
+print(f"\n{num_loop} inserts and gets done in {t2 - t1} seconds\n")
